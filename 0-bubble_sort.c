9aa6a221ef44388e97ef28efcb71879bfdcd1cb0
Sagebeme <93966131+sagebeme@users.noreply.github.com>
2023-01-24 20:37:11 +0300

    bubble sort algo change

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,25 @@
 #include "sort.h"
 
 /**
-* bubble_sort - a function that sorts data using the bubble sort algo
-* @array: an array of data to be sorted
-* @size: an unsigned integer
-* Return: nothing
-*
-* Description: the function prints the array after every iteration
-*/
+ * bubble_sort - a function that sorts data using the bubble sort algorithm
+ * @array: an array of data to be sorted
+ * @size: an unsigned integer
+ * Return: nothing
+ *
+ * Description: the function prints the array after every iteration
+ */
 
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int temp;
-	
+
 	if (array == NULL)
-			return;
-	
-	for (j = 0; j < size -1; j++)
+		return;
+
+	for (j = 0; j < size - 1; j++)
 	{
-		for (i = 0; j < size-1; j++)
+		for (i = 0; i < size - j - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
@@ -30,5 +30,4 @@ void bubble_sort(int *array, size_t size)
 			}
 		}
 	}
-
 }
