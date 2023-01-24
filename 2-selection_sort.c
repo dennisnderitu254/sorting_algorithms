cf2eed17f11241aefa53ab7ca12092bca15cb724
Sagebeme <93966131+sagebeme@users.noreply.github.com>
2023-01-24 20:50:04 +0300

    selection sort algo

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,44 +1,32 @@
 #include "sort.h"
 
 /**
- * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
- */
-void swap_ints(int *a, int *b)
-{
-	int tmp;
+* selection_sort - selection sort algorithm
+* @array: the array to sort
+* @size: the size of the array
+* Return: nothing
+*/
 
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
-
-/**
- * selection_sort - Sort an array of integers in ascending order
- *                  using the selection sort algorithm.
- * @array: An array of integers.
- * @size: The size of the array.
- *
- * Description: Prints the array after each swap.
- */
 void selection_sort(int *array, size_t size)
 {
-	int *min;
-	size_t i, j;
+	size_t i, j, position;
+	int swap;
 
-	if (array == NULL || size < 2)
-		return;
-
+	if (array == NULL)
+	        return;
 	for (i = 0; i < size - 1; i++)
 	{
-		min = array + i;
-		for (j = i + 1; j < size; j++)
-			min = (array[j] < *min) ? (array + j) : min;
-
-		if ((array + i) != min)
+		position = int max, i;
+		for (i = 0; i < size; j++)
 		{
-			swap_ints(array + i, min);
+			if (array[j] < array[position])
+			       position = j;
+		}
+		if (position != i)
+		{
+			swap = array[i];
+			array[i] = array[position];
+			array[position] = swap;
 			print_array(array, size);
 		}
 	}
